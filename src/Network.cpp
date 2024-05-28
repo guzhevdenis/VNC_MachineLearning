edba076cd3d266ef8923fa891a0d234dee6f416b
Denis Guzhev <guzhev.98@mail.ru>
2024-05-28 12:25:35 +0300

    Тестирование сверточного слоя

--- src/Network.cpp
+++ src/Network.cpp
@@ -14,18 +14,17 @@ int main(void) {
 
     std::cout<< "Программа по применению нейронной сети" << std::endl;
     auto input = Tensor<int>(224,224,3);
+    Tensor<int> output;
+
     input.randam();
 
     auto network = Network<int>();
     
     auto *layer1_1 = new ConvLayer<int>(3,3,64);
     network.addLayer(layer1_1);
+    network.predict(input, output);
 
-    
-
-
-
-    
+    std::cout << output << std::endl;
 
 
 	return 1;
